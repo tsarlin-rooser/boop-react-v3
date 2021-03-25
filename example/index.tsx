b367bc9fd902922931280f1b3533a0e23f25f825
Tsarlin Marie Domingo <tsarlin@Tsarlins-MacBook-Pro.local>
2021-03-25 12:24:11 +0000

    refactor: downgrade react to 16.8.3 same as RM

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,45 @@
-import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Thing, Button } from '../.';
 
 const App = () => {
+  const onClick = React.useCallback(() => alert('I am a button styled component!'), []);
   return (
-    <div>
+    <div className="ml-l">
+      <br />
       <Thing />
+      <br />
+      <br />
+      <div className="flex">
+        <div style={{ width: '20%'}}>
+          <Button text="primary" variant="primary" onClick={onClick} />
+          <br />
+          <br />
+          <br />
+          <Button text="secondary" variant="secondary" onClick={onClick} />
+          <br />
+          <br />
+          <br />
+          <Button text="tertiary" variant="tertiary" onClick={onClick} />
+          <br />
+          <br />
+          <br />
+          <Button text="destructive" variant="destructive" onClick={onClick} />
+        </div>
+        <div style={{ width: '20%'}}>
+          <Button text="primary" variant="primary" onClick={onClick} size="large" />
+          <br />
+          <br />
+          <Button text="secondary" variant="secondary" onClick={onClick} size="large" />
+          <br />
+          <br />
+          <Button text="tertiary" variant="tertiary" onClick={onClick} size="large" />
+          <br />
+          <br />
+          <Button text="destructive" variant="destructive" onClick={onClick} size="large" />
+        </div>
+      </div>
     </div>
   );
 };
